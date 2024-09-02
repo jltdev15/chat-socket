e1691a06b30195b47561b22c2207611ffc521b39
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2024-09-02 23:15:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const io = require("socket.io")(process.env.PORT, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  const user = users.find(
+    (user) => user.userId._value === userId || user.userId.value === userId
+  );
+  return user;
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    try {
+      const user = getUser(receiverId);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    } catch (err) {
+      console.log("friendId not found");
+    }
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
